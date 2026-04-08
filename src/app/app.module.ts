6da2b3dbf5955400930a1850425928739bf213bd
Irfan Farooq <irfan.farooq@novizant.com>
2026-04-08 16:35:56 +0500

    Test components removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TextComponentComponent } from './text-component/text-component.component';
-import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextComponentComponent,
-    ImagecomponentComponent
   ],
   imports: [
     BrowserModule,
